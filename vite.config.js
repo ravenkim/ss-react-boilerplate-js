52be0ad6ec76c94fa4e32285c38f513d81635399
ravenkim <41793830+ravenkim@users.noreply.github.com>
2025-03-21 14:23:10 +0900

    절대경로 설정 추가, 포트 변경 기능 추가

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // 포트지정
+  server: {
+    port: 6075,
+  },
+
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+    },
+  },
 })
